47b901aca896bdc84f4335e0630db97faed13d92
SCOTT P. GALLAGHER <sgallag@gmail.com>
2015-05-19 08:13:19 -0400

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,5 +50,5 @@ fi
 
 exec "$@"
 
-consul agent -server -join $JOIN_IP -data-dir /tmp/consul -config-dir /etc/consul.d/ &
+consul agent -server -join master -data-dir /tmp/consul -config-dir /etc/consul.d/ &
 mysqld --wsrep_cluster_address=gcomm://master
