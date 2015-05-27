7dfbe1b4bff36b715d08efe69c625c40812e8518
SCOTT P. GALLAGHER <sgallag@gmail.com>
2015-05-27 12:01:06 -0400

    no message

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 mysqld --wsrep-new-cluster &
-
+sleep 5
 mysql -uroot -e "CREATE DATABASE $MARIADB_DATABASE"
 mysql -uroot -e "CREATE USER $MARIADB_USER@'%' IDENTIFIED BY $MARIADB_PASSWORD"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO $MARIADB_USER@'%' WITH GRANT OPTION"
 
 mysqladmin -uroot shutdown
+
